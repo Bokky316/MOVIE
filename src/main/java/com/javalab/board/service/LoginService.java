ce535686734772b53302758512b3c54a2ffd94b7
Bokky <Bokky316@gmail.com>
2024-12-14 13:07:18 +0900

    no message

--- src/main/java/com/javalab/board/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.javalab.board.service;
-
-import com.javalab.board.vo.MemberVo;
-
-public interface LoginService {
-	public MemberVo checkLogin(String memberId, String password);
-
-}
--- src/main/java/com/javalab/board/vo/RoleVo.java
+++
@@ -1,9 +0,0 @@
-package com.javalab.board.vo;
-
-import lombok.Data;
-
-@Data
-public class RoleVo {
-    private String roleId; // 역할 ID
-    private String roleName; // 역할 이름
-}
